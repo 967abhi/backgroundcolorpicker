b78212c44f37e9535f736b7ae4970e7fa46d2522
Abhishek kumar singh <86023413+967abhi@users.noreply.github.com>
2022-06-10 16:38:22 +0530

    firstcommit

---
+++ littledemo.js
@@ -0,0 +1,15 @@
+const mainButton = document.querySelector("button");
+const body=document.body;
+const currentColor=document.querySelector(".current-color")
+function randomColorGenerator(){
+    const red = Math.floor(Math.random()*256);
+    const green= Math.floor(Math.random()*256);
+    const blue = Math.floor(Math.random()*256);
+    const randomcolor =` rgb(${red},${green},${blue})`
+    return randomcolor;
+}
+mainButton.addEventListener("click",()=>{
+    const randomcolor= randomColorGenerator();
+    body.style.backgroundColor=randomcolor;
+    currentColor.textContent=randomcolor;
+})
